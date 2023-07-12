c53845e809d3a2bed41d824b27ee7f2d9af800ee
Pavithra12Gowda <Pavithra12Gowda>
2023-07-12 14:20:35 +0530

    adding Swiggy files crud

---
+++ Swiggy.java
@@ -0,0 +1,172 @@
+class Swiggy{
+
+	public static double search(String foodName){
+		
+		if("Maggi"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 39.00;
+		}
+		if("Roti"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 20.00;
+		}
+		if("Rice"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 76.00;
+		}
+		if("Poha"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 90.00;
+		}
+		if("Egg curry"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 100.00;
+		}
+		if("Veg noodle"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 28.00;
+		}
+		if("tomoto rice"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 500.00;
+		}
+		if("Dal fry"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 359.00;
+		}
+		if("jeera rice"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 20.00;
+		}
+		if("fish curry"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 90.00;
+		}
+		if("chill potato"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 500.00;
+		}
+		if("veg soup"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 140.00;
+		}
+		if("paneer"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 55.00;
+		}
+		if("curd rice"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 200.00;
+		}
+		if("thai rice"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 80.00;
+		}
+		if("fish fry"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 88.00;
+		}
+		if("soda"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 91.00;
+		}
+		if("doi"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 703.00;
+		}
+		if("pani puri"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 100.00;
+		}
+		if("badam milk"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 70.00;
+		}
+		return 0.0;
+	}
+	public static double search(String foodName,int quantity){
+		
+		if("Maggi"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 39.00*quantity;
+		}
+		if("Roti"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 20.00*quantity;
+		}
+		if("Rice"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 76.00*quantity;
+		}
+		if("Poha"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 90.00*quantity;
+		}
+		if("Egg curry"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 100.00*quantity;
+		}
+		if("Veg noodle"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 28.00*quantity;
+		}
+		if("tomoto rice"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 500.00*quantity;
+		}
+		if("Dal fry"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 359.00*quantity;
+		}
+		if("jeera rice"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 20.00*quantity;
+		}
+		if("fish curry"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 90.00*quantity;
+		}
+		if("chill potato"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 500.00*quantity;
+		}
+		if("veg soup"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 140.00*quantity;
+		}
+		if("paneer"==foodName){
+		//System.out.println("Seached food name is "+foodName);
+		return 55.00*quantity;
+		}
+		if("curd rice"==foodName){
+		//System.out.println("Seached food name is "+foodName);
+		return 200.00*quantity;
+		}
+		if("thai rice"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 80.00*quantity;
+		}
+		if("fish fry"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 88.00*quantity;
+		}
+		if("soda"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 91.00*quantity;
+		}
+		if("doi"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 703.00*quantity;
+		}
+		if("pani puri"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 100.00*quantity;
+		}
+		if("badam milk"==foodName){
+		System.out.println("Seached food name is "+foodName);
+		return 70.00*quantity;
+		}
+		return 0.0;
+	}
+
+}
